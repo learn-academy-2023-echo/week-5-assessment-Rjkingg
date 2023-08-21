78cd5efcc04a225f59935e4dfd5d36ae55af0a8f
raquel king <kingraq1007@gmail.com>
2023-08-21 10:13:53 -0500

    week 5 completed

--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,49 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. Why would super be used in a Ruby class?
 
-Your answer:
+Your answer: In a Ruby class, super would be used to create a sub class.
 
-Researched answer:
+Researched answer: Super is a keyword that gets called within a parent class and searches for another method with the same name as the parent
 
 2. What is a gem?
 
-Your answer:
+Your answer: A gem in Ruby is a library
 
-Researched answer:
+Researched answer: A gem in Ruby is a package manager that allows you to access methods that were created by other developers
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: A relational database is a database that is directly related to a particular programming language. There are multiple relational databases.
 
-Researched answer:
+Researched answer: A relational database is stores information. There are tables, rows and columns, each connected in some way.
 
 4. What are primary keys? Why purpose does a primary key serve?
 
-Your answer:
+Your answer: Primary keys, in Rails are the numbers associated with each column, its primary purpoise is allowing the different columns to have different ID's
 
-Researched answer:
+Researched answer: When data in a database is stored, each column has a primary key, and this ensures that each record is unique and distinctive 
 
 5. What are the five HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: Get, post, put, delete and patch
 
-Researched answer:
+Researched answer: HTTP verbs are  commands for web interactions. The main ones are:
+GET - "Read" data.
+POST - "Create" new data.
+PUT - "Update" existing data.
+DELETE - "Delete" data.
+PATCH - Partially modify data.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. Model validations:
+1. Model validations: Model validation takes place after Model Training. In this stage, the trained model is assessed using a testing dataset. This testing dataset can come from the same dataset used for training or a different one.
 
-2. RESTful routes:
+2. RESTful routes: RESTful routes are predefined web addresses that help manage resources (such as data) within a web application. These routes simplify tasks like adding, retrieving, updating, and removing data by utilizing the right web address and HTTP actions (like GET, POST, PUT, DELETE).
 
-3. ERB:
+3. ERB: ERB stands for Embedded Ruby. It's a technology used in web development to combine Ruby code with HTML or other markup languages. ERB allows dynamic content and logic to be seamlessly integrated into static web pages, making it easier to create interactive and data-driven websites.
 
-4. Params:
+4. Params: Params refer to parameters in programming. They are pieces of information passed to functions, methods, or programs to influence their behavior or provide input.
 
-5. API:
+5. API: API, which stands for Application Programming Interface, is a tool that enables different software applications to communicate and interact with each other.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -7,10 +7,35 @@ beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
 
 filter_letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
-filter_letter_t = 't'
+# filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+def words_with_target_letter(words_array, target_letter)
+    filtered_words = []
+  
+    words_array.each do |word|
+      if word.include?(target_letter)
+        filtered_words.push(word)
+      end
+    end
+  
+    return filtered_words
+  end
+
+
+beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+
+filter_letter_o = 'o'
+result_o = words_with_target_letter(beverages_array, filter_letter_o)
+puts result_o.inspect
+
 # Pseudo code:
+#define a function that takes two parameters
+#Initialize an empty array (this will holds words that contain target letter)
+#each method will loop through each word
+#use .include method (to see if word contains the target letter)
+#return filtered_words array (contains words included in the target letter)
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -18,13 +43,60 @@ filter_letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+
+def hash_values_in_alphabetical_order(hash)
+    values = hash.values.flatten.sort
+    return values
+  end
+  
+  us_states = { 
+    northwest: ['Washington', 'Oregon', 'Idaho'], 
+    southwest: ['California', 'Arizona', 'Nevada'], 
+    notheast: ['Maine', 'New Hampshire', 'Rhode Island'] 
+  }
+  
+  result = hash_values_in_alphabetical_order(us_states)
+  puts result.inspect
+  
+
+
+
 # Pseudo code:
+# define a function that takes in parameter hash
+# Initialize empty array that will hold values from nested arrays
+# iterate through each value array in the hash using each method
+# use .push method to flatten the nested arrays
+# use .sort method to sort alphabetically 
+# return the values 
+
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+class Bike
+    def initialize(model, wheels = 2, current_speed = 0)
+      @model = model
+      @wheels = wheels
+      @current_speed = current_speed
+    end
+  
+    def bike_info
+      "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+  end
+  
+ 
+  my_bike = Bike.new('Trek')
+  puts my_bike.bike_info
+  
+
 # Pseudo code:
+# define class Bike
+# create initialize method that takes 3 parameters
+# set instance variables
+# define method bike_info
+# return formatted string with bike info using instance variables
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -33,4 +105,38 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
 
+class Bike
+    def initialize(model, wheels = 2, current_speed = 0)
+      @model = model
+      @wheels = wheels
+      @current_speed = current_speed
+    end
+  
+    def bike_info
+      "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+  
+    def pedal_faster(amount)
+      @current_speed += amount
+    end
+  
+    def brake(amount)
+        @current_speed = [@current_speed - amount, 0].max
+    end
+  end
+  
+  
+  puts my_bike.pedal_faster(10)
+  puts my_bike.pedal_faster(18)
+  puts my_bike.brake(5)
+  puts my_bike.brake(25)
+  
 # Pseudo code:
+# define class bike
+# create and initializemethod that takes 3 parameters
+# set instance variables 
+# define method bike_info
+# return formatted string with bike info using instance 
+# define method pedal_faster that takes 1 parameter
+# calculate new speed by subtracting amount from current_speed
+# set current_speed to max of new_speed and 0
